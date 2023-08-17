588e3dc52b246f6fd5782b33aca7560df4b6da32
Okellitto <okellitto@gmail.com>
2023-08-16 20:13:24 -0700

    Learn the times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,11 @@
  */
 
 void times_table(void)
+
 {
 	int x, y, z, u, d;
-	for(x =0; x <= 9; x++)
+
+	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
@@ -19,12 +21,12 @@ void times_table(void)
 				d = (z - u) / 10;
 				_putchar(44);
 				_putchar(32);
-				_putchar(d +'0');
+				_putchar(d + '0');
 				_putchar(u + '0');
 			}
 			else
 			{
-				if (y !=0)
+			if (y != 0)
 				{
 					_putchar(44);
 					_putchar(32);
