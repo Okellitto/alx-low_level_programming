3aaf41cfbaf1558838454b293bce15c2ddcb0e4e
Okellitto <okellitto@gmail.com>
2023-08-22 04:19:10 -0700

    Dont swap horse in crossing a stream

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+ * _atoi - function that convert a string to an integer
+ * Return: Always 0
+ */
+
+int _atoi(char *s){
+	char num1[] = " "
+		char num2[] = " "
+
+		int number 1, number 2;
+	
+	number1 = atoi(num1);
+	number2 = atoi(num2);
+
+
